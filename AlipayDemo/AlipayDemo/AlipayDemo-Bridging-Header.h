678cb530f13526f1278a2c0c07f35cb0bfd79fa5
张青明 <zhangqingming@codinghacker.com>
2015-09-09 23:44:43 +0800

    桥接文件

---
+++ AlipayDemo/AlipayDemo/AlipayDemo-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  AlipayDemo-Bridging-Header.h
+//  AlipayDemo
+//
+//  Created by 张青明 on 15/9/8.
+//  Copyright (c) 2015年 极客栈. All rights reserved.
+//
+
+#import "Order.h"
+
+
+#import "DataSigner.h"
+
+
+#import <AlipaySDK/AlipaySDK.h>
